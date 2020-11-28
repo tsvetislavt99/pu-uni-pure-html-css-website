d45a5011d305277f0d1073c11927e2ce8187799a
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2020-11-28 13:56:40 +0200

    Added folders

--- README.md
+++
@@ -1,5 +0,0 @@
-# pu-uni-pure-html-css-website
-A website for our PBL project written in pure HTML and CSS, as the idea was that a high school student should be able to do something like this.
-
-
-https://tsvetislavt99.github.io/pu-uni-pure-html-css-website/
---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+myButton = document.querySelector(".scroll-top-btn");
+
+console.log(myButton);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
